5f4461320bb47733c79f50b56c0362dd1c56a286
Emmanuel Adedire <adedireemmanuel@gmail.com>
2025-06-04 14:46:33 +0100

    Added assets icons and logo

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -1,1 +1,25 @@
-class Assets {}
+class Assets {
+  Assets._();
+
+  static final images = _AssetsImages._();
+  static final svg = _AssetsSvg._();
+}
+
+class _AssetsImages {
+  _AssetsImages._();
+
+  final logoPNG = 'assets/images/logo.png';
+}
+
+class _AssetsSvg {
+  _AssetsSvg._();
+
+  final addSVG = 'assets/svg/add.svg';
+  final cartSVG = 'assets/svg/cart.svg';
+  final homeSVG = 'assets/svg/home.svg';
+  final likeSVG = 'assets/svg/like.svg';
+  final minusSVG = 'assets/svg/minus.svg';
+  final notificationSVG = 'assets/svg/notification.svg';
+  final searchSVG = 'assets/svg/search.svg';
+  final trashSVG = 'assets/svg/trash.svg';
+}
